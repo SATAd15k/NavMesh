656647ad0b41785df6ece3cbe69a9c87e0bf9ee6
SATAd15k <140038492+SATAd15k@users.noreply.github.com>
2023-07-27 15:32:55 +1000

    Agent follows mouse location when pressed down
    Added some code and some explanation on how the agent capsule will path to mouse when left click is pressed

--- Assets/Scripts/MoveToObject.cs
+++ Assets/Scripts/MoveToObject.cs
@@ -16,6 +16,14 @@ public class MoveToObject : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetMouseButton(0)) // This is left click; changed to one would be right click
+        {
+            MoveAgentToPoint(true); // Move agent to where left click is pressed when pressed down
+        }
+    }
+    void MoveAgentToPoint(bool isFollowMouse) // boolean; true or false statement
+    {
+
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //Cheap way of running a ray from the camera
 
         // Get information about the collider the raycast hit
@@ -23,10 +31,16 @@ public class MoveToObject : MonoBehaviour
 
         // Run the raycast
         if (Physics.Raycast(ray.origin, ray.direction, out hitinfo)) // Has to hit a collider or wont recognise anything. If it hits something it will enter the if statement below
+        {
+            if (isFollowMouse)
             {
-
+                _agent.destination = hitinfo.point;
             }
-
-        _agent.destination = moveToObject.transform.position;
+            else
+            {
+                // Follow the game obj (in this case the sphere)
+                _agent.destination = moveToObject.transform.position;
+            }
+        }
     }
 }
